fd4f6e8d697e71f42fdc9e25aac047fdf9d99f9e
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-04-08 18:59:15 +0000

    Completed finally

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,79 +1,38 @@
 public class PracticeProblem {
-    public static int sum2D(int[][] array) {
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) { 
-            for (int j = 0; j < array[i].length; j++) { 
-                sum += array[i][j];
-            }
-        }
-        return sum;
-    }
-    
+
     public static boolean validIndex(int[] array, int index) {
         try {
-            int temp = array[index]; 
-            return true;
+            return index >= 0 && index < array.length;
         } catch (ArrayIndexOutOfBoundsException e) {
             return false;
         }
     }
-    
-    public static void main(String[] args) {
-        int[][] numbers = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
-        
-        System.out.println("Sum of elements: " + sum2D(numbers)); 
-        
-        int[] sampleArray = {10, 20, 30, 40};
-        System.out.println("Is index 2 valid? " + validIndex(sampleArray, 2)); 
-        System.out.println("Is index 5 valid? " + validIndex(sampleArray, 5)); 
-    }
-}
-public class PracticeProblem {
-    public static int sum2D(int[][] array) {
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) { // 
-            for (int j = 0; j < array[i].length; j++) { // 
-                sum += array[i][j];
-            }
-        }
-        return sum;
-    }
-    
-    public static boolean validIndex(int[] array, int index) {
+
+    public static int divide(int numerator, int denominator) {
         try {
-            int temp = array[index]; 
-            return true;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return false;
-        }
-    }
-    
-    public static int divide(int a, int b) {
-        try {
-            return a / b;
+            return numerator / denominator;
         } catch (ArithmeticException e) {
             return 0;
         }
     }
-    
+
+    public static int safeConvertStringtoInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
-        int[][] numbers = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
-        
-        System.out.println("Sum of elements: " + sum2D(numbers)); 
-        
-        int[] sampleArray = {10, 20, 30, 40};
-        System.out.println("Is index 2 valid? " + validIndex(sampleArray, 2)); 
-        System.out.println("Is index 5 valid? " + validIndex(sampleArray, 5)); 
-        
-        System.out.println("10 / 2 = " + divide(10, 2)); 
-        System.out.println("10 / 0 = " + divide(10, 0)); 
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(validIndex(array, 2));
+        System.out.println(validIndex(array, 5)); 
+
+        System.out.println(divide(10, 2)); 
+        System.out.println(divide(10, 0)); 
+
+        System.out.println(safeConvertStringtoInt("123")); 
+        System.out.println(safeConvertStringtoInt("abc")); 
     }
 }
